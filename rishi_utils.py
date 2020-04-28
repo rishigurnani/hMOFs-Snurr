27ef80b2c9a7acf548d57b748729b4ff32152939
Rishi Gurnani <rgurnani96@gmail.com>
2020-04-28 17:04:10 -0400

    added to rishi_utils

--- rishi_utils.py
+++ rishi_utils.py
@@ -1,7 +1,33 @@
 import pandas as pd
+import sys
+import argparse
+import gzip
 
 def pd_load(path):
     try:
         return pd.read_csv(path)
     except:
         return pd.read_csv(path, compression='gzip')
+
+def compress_gzip(compress):
+    '''
+    Compress all files in the list 'compress'
+    '''
+    for path in compress:
+        print('Compressing %s' %path)
+        df = pd.read_csv(path)
+
+        df.to_csv(path, compression='gzip')
+     
+def count_lines(filename):
+    '''
+    Count lines in file for gzip or not gzip
+    '''
+    try: #normal
+        with open(filename, 'r') as handle:
+            n_lines = sum(1 for row in handle)
+    except: #gzip
+        with gzip.open(filename, 'rb') as handle:
+            n_lines = sum(1 for row in handle)
+          
+    return n_lines
